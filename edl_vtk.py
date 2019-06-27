9069e05ad95c3901ba495677b7eb2251aa88b70e
Guillaume Favelier <guillaume.favelier@gmail.com>
2019-06-27 16:58:41 +0200

    Update python vtk version

--- edl_vtk.py
+++ edl_vtk.py
@@ -1,18 +1,43 @@
-import pyvista as pv
 import vtk
+
+# PyVista version
+# import pyvista as pv
+# sphere = vtk.vtkSphereSource()
+# sphere.Update()
+# mesh = sphere.GetOutput()
+# p = pv.Plotter(shape=(1, 2))
+# p.subplot(0, 0)
+# p.add_mesh(mesh, color=True)
+# p.subplot(0, 1)
+# p.add_mesh(mesh, color=True)
+# p.enable_eye_dome_lighting((0, 1))
+# p.show()
+
+# VTK version
+renderer = vtk.vtkRenderer()
+renderWindow = vtk.vtkRenderWindow()
+renderWindow.AddRenderer(renderer)
+iren = vtk.vtkRenderWindowInteractor()
+iren.SetRenderWindow(renderWindow)
 
 sphere = vtk.vtkSphereSource()
 sphere.Update()
 mesh = sphere.GetOutput()
 
-p = pv.Plotter(notebook=False, shape=(1, 2))
+mapper = vtk.vtkPolyDataMapper()
+mapper.SetInputData(sphere.GetOutput())
 
-p.subplot(0, 0)
-p.add_mesh(mesh, color=True)
+actor = vtk.vtkActor()
+actor.SetMapper(mapper)
 
-p.subplot(0, 1)
-p.add_mesh(mesh, color=True)
+renderer.AddActor(actor)
 
-p.enable_eye_dome_lighting((0, 1))
+basicPasses = vtk.vtkRenderStepsPass()
 
-p.show()
+edl = vtk.vtkEDLShading()
+edl.SetDelegatePass(basicPasses)
+
+renderer.SetPass(edl)
+
+renderWindow.Render()
+iren.Start()
