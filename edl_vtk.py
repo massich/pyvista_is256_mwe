c79f115e530cdb5029cc2e3171ad5d750878a9ba
Guillaume Favelier <guillaume.favelier@gmail.com>
2019-06-27 18:52:44 +0200

    update python vtk version

--- edl_vtk.py
+++ edl_vtk.py
@@ -1,14 +1,9 @@
 import vtk
 
 # VTK version
-renderer1 = vtk.vtkRenderer()
-renderer2 = vtk.vtkRenderer()
 renderWindow = vtk.vtkRenderWindow()
 renderWindow.SetSize(500, 500)
-renderWindow.AddRenderer(renderer1)
-renderer1.SetViewport(0, 0, 0.5, 1.0)
-renderWindow.AddRenderer(renderer2)
-renderer2.SetViewport(0.5, 0, 1.0, 1.0)
+
 iren = vtk.vtkRenderWindowInteractor()
 iren.SetRenderWindow(renderWindow)
 
@@ -22,17 +17,43 @@ mapper.SetInputData(sphere.GetOutput())
 actor = vtk.vtkActor()
 actor.SetMapper(mapper)
 
-renderer1.AddActor(actor)
-renderer2.AddActor(actor)
 
+def get_cam():
+    lights = vtk.vtkLightsPass()
+    default = vtk.vtkDefaultPass()
+
+    passes = vtk.vtkRenderPassCollection()
+    passes.AddItem(lights)
+    passes.AddItem(default)
+
+    seq = vtk.vtkSequencePass()
+    seq.SetPasses(passes)
+
+    cameraP = vtk.vtkCameraPass()
+    cameraP.SetDelegatePass(seq)
+    return cameraP
+
+
+def get_edl():
+    edl = vtk.vtkEDLShading()
+    edl.SetDelegatePass(get_cam())
+    return edl
+
+
+renderer1 = vtk.vtkRenderer()
+renderer1.SetViewport(0, 0, 0.5, 1.0)
+renderer1.SetPass(get_edl())
+renderer1.AddActor(actor)
 renderer1.ResetCamera()
+
+renderer2 = vtk.vtkRenderer()
+renderer2.SetViewport(0.5, 0, 1.0, 1.0)
+renderer2.SetPass(get_edl())
+renderer2.AddActor(actor)
 renderer2.ResetCamera()
 
-basicPasses = vtk.vtkRenderStepsPass()
-
-edl = vtk.vtkEDLShading()
-edl.SetDelegatePass(basicPasses)
-renderer2.SetPass(edl)
+renderWindow.AddRenderer(renderer1)
+renderWindow.AddRenderer(renderer2)
 
 renderWindow.Render()
 iren.Start()
